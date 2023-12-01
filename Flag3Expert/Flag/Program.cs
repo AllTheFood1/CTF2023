c042d43ad316d2e47fcadec64851baf661b9af6a
allthefood <skiergraham5280@gmail.com>
2023-11-30 17:50:54 -0700

    Working on expert flag 3

---
+++ Flag3Expert/Flag/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] pname = Process.GetProcessesByName("TieFighter");
+            if (pname.Length == 0)
+            {
+                Console.WriteLine("This is the password");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Flag3Expert/TieFIghter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
